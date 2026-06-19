e0c7d8282b1c33378b62da21d5fd60824cd6c44d
este102022 <112830238+este102022@users.noreply.github.com>
2026-06-19 21:16:15 +0100

    Eunibeam website

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//slideshow animation + functionality
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove("active");
+
+  current = (current + 1) % slides.length;
+
+  slides[current].classList.add("active");
+}, 4000);
